ff2b535476a6c9b0d088d310007c06320a9c1ef7
linuskelsey <alfred.linus.kelsey@gmail.com>
2026-02-16 13:12:09 +0000

    strip functions.py of unused functions

--- lib/functions.py
+++ lib/functions.py
@@ -16,82 +16,6 @@ mylogger = logging.getLogger(__name__)
 '''
 
 
-# General functions/Quantum programs
-
-# Z Rotation operators 
-theta = np.pi/8
-# 16 types of rotations
-R22 =create_rotation_op(   theta, rotation_axis=(0, 0, 1))
-R45 =create_rotation_op( 2*theta, rotation_axis=(0, 0, 1))
-R67 =create_rotation_op( 3*theta, rotation_axis=(0, 0, 1))
-R90 =create_rotation_op( 4*theta, rotation_axis=(0, 0, 1))
-R112=create_rotation_op( 5*theta, rotation_axis=(0, 0, 1))
-R135=create_rotation_op( 6*theta, rotation_axis=(0, 0, 1))
-R157=create_rotation_op( 7*theta, rotation_axis=(0, 0, 1))
-R180=create_rotation_op(   np.pi, rotation_axis=(0, 0, 1))
-R202=create_rotation_op( 9*theta, rotation_axis=(0, 0, 1))
-R225=create_rotation_op(10*theta, rotation_axis=(0, 0, 1))
-R247=create_rotation_op(11*theta, rotation_axis=(0, 0, 1))
-R270=create_rotation_op(12*theta, rotation_axis=(0, 0, 1))
-R292=create_rotation_op(13*theta, rotation_axis=(0, 0, 1))
-R315=create_rotation_op(14*theta, rotation_axis=(0, 0, 1))
-R337=create_rotation_op(15*theta, rotation_axis=(0, 0, 1))
-
-#============================================================
-
-INSTR_R22 = IGate('Z Rotated 22.5',operator=R22)
-INSTR_R45 = IGate('Z Rotated 45'  ,operator=R45)
-INSTR_R67 = IGate('Z Rotated 67.5',operator=R67)
-INSTR_R90 = IGate('Z Rotated 90'    ,operator=R90)
-INSTR_R112 = IGate('Z Rotated 112.5',operator=R112)
-INSTR_R135 = IGate('Z Rotated 135'  ,operator=R135)
-INSTR_R157 = IGate('Z Rotated 157.5',operator=R157)
-INSTR_R180 = IGate('Z Rotated 180  ',operator=R180)
-#------------------------------------------------------------
-INSTR_R202 = IGate('Z Rotated 202.5',operator=R202)
-INSTR_R225 = IGate('Z Rotated 225  ',operator=R225)
-INSTR_R247 = IGate('Z Rotated 247.5',operator=R247)
-INSTR_R270 = IGate('Z Rotated 270  ',operator=R270)
-INSTR_R292 = IGate('Z Rotated 292.5',operator=R292)
-INSTR_R315 = IGate('Z Rotated 315  ',operator=R315)
-INSTR_R337 = IGate('Z Rotated 337.5',operator=R337)
-#============================================================
-INSTR_Rv22 = IGate('Z Rotated -22.5',operator=R22.inv)
-INSTR_Rv45 = IGate('Z Rotated -45'  ,operator=R45.inv)
-INSTR_Rv67 = IGate('Z Rotated -67.5',operator=R67.inv)
-INSTR_Rv90 = IGate('Z Rotated -90'    ,operator=R90.inv)
-INSTR_Rv112 = IGate('Z Rotated -112.5',operator=R112.inv)
-INSTR_Rv135 = IGate('Z Rotated -135'  ,operator=R135.inv)
-INSTR_Rv157 = IGate('Z Rotated -157.5',operator=R157.inv)
-INSTR_Rv180 = IGate('Z Rotated -180  ',operator=R180.inv)
-#------------------------------------------------------------
-INSTR_Rv202 = IGate('Z Rotated -202.5',operator=R202.inv)
-INSTR_Rv225 = IGate('Z Rotated -225  ',operator=R225.inv)
-INSTR_Rv247 = IGate('Z Rotated -247.5',operator=R247.inv)
-INSTR_Rv270 = IGate('Z Rotated -270  ',operator=R270.inv)
-INSTR_Rv292 = IGate('Z Rotated -292.5',operator=R292.inv)
-INSTR_Rv315 = IGate('Z Rotated -315  ',operator=R315.inv)
-INSTR_Rv337 = IGate('Z Rotated -337.5',operator=R337.inv)
-
-
-# used by AnonymousTransmission
-NToffoli_matrix = [[0, 1, 0, 0, 0, 0, 0, 0],
-                   [1, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 1, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 1, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 1, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 1, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 1, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 1]]
-
-Operator_NToffoli = Operator(name='Operator_NToffoli', matrix=NToffoli_matrix)
-INSTR_NToffoli = IGate('INSTR_NToffoli',operator=Operator_NToffoli)
-
-
-
-
-
-
 
 '''
 A function used to model fibre loss after keys were formed in QKD protocols.
@@ -165,67 +89,12 @@ def bitFlipNoice(bit,f0,f1,randomInteger):
 
 
 
-
 '''
 Simply returns a list with 0 or 1 in given length.
 '''
 def Random_basis_gen(length):
     return [randint(0,1) for i in range(length)]
 
-
-'''
-Apply rotation on qubits.
-input:
-    locationIndex: list of int, Index of qubits to rotate.
-    rotationIndex: list of int each element is limited in [-7,7], indecating -315...,-45,0,45,...270,315 degree.
-'''
-class RotateQubits(QuantumProgram):
-    def __init__(self,locationIndex,rotationIndex):
-        self.locationIndex=locationIndex
-        self.rotationIndex=rotationIndex
-        super().__init__()
-        #print("RotateQubits locationIndex:",self.locationIndex)
-        #print("RotateQubits rotationIndex:",self.rotationIndex)
-
-    def program(self):
-        
-        for loc, rot in zip(self.locationIndex,self.rotationIndex):
-            #print("loc, rot ",loc," : ",rot)
-            if rot == 1:
-                self.apply(INSTR_R45, qubit_indices=loc,physical=True)
-            elif rot==2:
-                self.apply(INSTR_R90, qubit_indices=loc,physical=True)
-            elif rot== 3:
-                self.apply(INSTR_R135, qubit_indices=loc,physical=True)
-            elif rot== 4:
-                self.apply(INSTR_R180, qubit_indices=loc,physical=True)
-            elif rot== 5:
-                self.apply(INSTR_R225, qubit_indices=loc,physical=True)
-            elif rot== 6:
-                self.apply(INSTR_R270, qubit_indices=loc,physical=True)
-            elif rot== 7:
-                self.apply(INSTR_R315, qubit_indices=loc,physical=True)
-            elif rot == -1:
-                self.apply(INSTR_Rv45, qubit_indices=loc,physical=True)
-            elif rot== -2:
-                self.apply(INSTR_Rv90, qubit_indices=loc,physical=True)
-            elif rot== -3:
-                self.apply(INSTR_Rv135, qubit_indices=loc,physical=True)
-            elif rot== -4:
-                self.apply(INSTR_Rv180, qubit_indices=loc,physical=True)
-            elif rot== -5:
-                self.apply(INSTR_Rv225, qubit_indices=loc,physical=True)
-            elif rot== -6:
-                self.apply(INSTR_Rv270, qubit_indices=loc,physical=True)
-            elif rot== -7:
-                self.apply(INSTR_Rv315, qubit_indices=loc,physical=True)
-            elif rot== 0:
-                pass
-            else:
-                print("ERROR!!!  ============= Rotation index Error!! Index:",rot)
-                #mylogger.error("Rotation index Error!! Index: {} ".format(j))
-                
-        yield self.run(parallel=False)
 
 
 '''
@@ -420,67 +289,6 @@ class QMeasureByPosition(QuantumProgram):
         yield self.run(parallel=False)
 
 
-'''
-input:
-    positionInx:int List : Index in Qmem to measure.
-    angleInx:int List (each value from 0 to 8): Index indecating measurement angle along Z-axis. 
-    Corresponding to degree 0, 45, 90, ...315.
-output:
-
-'''
-class AngleMeasure(QuantumProgram):
-    def __init__(self,positionInx,angleInx):
-        self.positionInx=positionInx
-        self.angleInx=angleInx
-        super().__init__()
-
-    def program(self):
-
-        for pos,angle in zip(self.positionInx,self.angleInx):
-
-            # make sure angle is in the acceptable range
-            while angle<0:
-                angle+=8
-            while angle>=8:
-                angle-=8
-
-            #print("Function angle:",angle)
-            
-            if   angle == 1:
-                self.apply(INSTR_Rv45,pos)
-                self.apply(INSTR_MEASURE_X,qubit_indices=pos, output_key=str(pos),physical=True)
-                self.apply(INSTR_R45,pos)
-            elif angle == 2:
-                self.apply(INSTR_Rv90,pos)
-                self.apply(INSTR_MEASURE_X,qubit_indices=pos, output_key=str(pos),physical=True)
-                self.apply(INSTR_R90,pos)
-            elif angle == 3:
-                self.apply(INSTR_Rv135,pos)
-                self.apply(INSTR_MEASURE_X,qubit_indices=pos, output_key=str(pos),physical=True)
-                self.apply(INSTR_R135,pos)
-            elif angle== 4:
-                self.apply(INSTR_Rv180,pos)
-                self.apply(INSTR_MEASURE_X,qubit_indices=pos, output_key=str(pos),physical=True)
-                self.apply(INSTR_R180,pos)
-            elif angle== 5:
-                self.apply(INSTR_Rv225,pos)
-                self.apply(INSTR_MEASURE_X,qubit_indices=pos, output_key=str(pos),physical=True)
-                self.apply(INSTR_R225,pos)
-            elif angle== 6:
-                self.apply(INSTR_Rv270,pos)
-                self.apply(INSTR_MEASURE_X,qubit_indices=pos, output_key=str(pos),physical=True)
-                self.apply(INSTR_R270,pos)
-            elif angle== 7:
-                self.apply(INSTR_Rv315,pos)
-                self.apply(INSTR_MEASURE_X,qubit_indices=pos, output_key=str(pos),physical=True)
-                self.apply(INSTR_R315,pos)
-            
-            else:  # angle== 0
-                self.apply(INSTR_MEASURE_X,qubit_indices=pos, output_key=str(pos),physical=True)
-        
-        
-        yield self.run(parallel=False)
-
 
 
 
@@ -523,43 +331,11 @@ class QCZ(QuantumProgram):
         
  
 
-
-
-
-
-
 def logical_xor(str1, str2):
     return bool(str1) ^ bool(str2) 
 
 
 
-
-
-
-class makeWstate(QuantumProgram):
-    
-    def __init__(self,numQubits=4):
-        self.numQubits=numQubits
-        super().__init__()
-        
-    def program(self):
-        if self.numQubits%4==0:
-            self.apply(INSTR_H, 2)
-            self.apply(INSTR_H, 3)
-            self.apply(INSTR_NToffoli, [3,2,1])
-            self.apply(INSTR_TOFFOLI,qubit_indices=[3,2,0],physical=True) 
-            self.apply(INSTR_CNOT, [0, 2])
-            self.apply(INSTR_CNOT, [0, 3])
-        else:
-            print("numbers of qubits should be a multiple of 4")
-        
-        yield self.run(parallel=False)
-        
-
-
 def ProgramFail(info):
     print(info)
-    print("programe failed!!")    
-    
-
-    
+    print("Programme failed.")
