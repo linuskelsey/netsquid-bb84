2e2f65e5b34092f7bded131ad8cbd721aecec2e2
linuskelsey <alfred.linus.kelsey@gmail.com>
2026-02-18 11:42:20 +0000

    all cleared

--- BB84/BB84_Alice.py
+++
@@ -1,8 +0,0 @@
-import sys
-scriptpath = "lib/"
-sys.path.append(scriptpath)
-from lib.functions import Random_basis_gen
-
-import logging
-logging.basicConfig(level=logging.INFO)
-mylogger = logging.getLogger(__name__)
