eb5d9e85f4a5ec91c3da27ce436d3132ec5fac38
linuskelsey <alfred.linus.kelsey@gmail.com>
2026-02-25 16:01:54 +0000

    initialise MDI

---
+++ MDI/mdiRelayNode.py
@@ -0,0 +1,22 @@
+import netsquid as ns
+
+from netsquid.protocols import NodeProtocol
+from netsquid.components.qsource import SourceStatus
+
+import sys
+scriptpath = "lib/"
+sys.path.append(scriptpath)
+from lib.functions import rng_bin_lst
+
+
+
+class RelayNodeProtocol(NodeProtocol):
+    """
+    Protocol to run on End User node in point-to-point BB84.
+
+    Attributes:
+        
+
+    Parameters:
+        
+    """
